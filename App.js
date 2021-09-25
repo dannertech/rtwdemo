9efce5974d088249d9c69ad2861c81d5f2a6441d
Diamonique Danner <diamoniquedanner@Diamoniques-MacBook-Pro.local>
2021-09-24 22:17:15 -0400

    Added navigation

--- App.js
+++ App.js
@@ -1,6 +1,37 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './src/Screens/Home';
+import BagScreen from './src/Screens/Bag';
+import BrowseScreen from './src/Screens/Browse';
+import HeartsScreen from './src/Screens/Hearts';
+import MyRTRScreen from './src/Screens/MyRTR';
 
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} options={
+          {
+            tabBarIcon: () => {
+              return(
+                <Icon name="home-outline" size={24} />
+              )
+            }
+          }
+        }
+        />
+        <Tab.Screen name="Browse" component={BrowseScreen} />
+        <Tab.Screen name="Bag" component={BagScreen} />
+        <Tab.Screen name="Hearts" component={HeartsScreen} />
+        <Tab.Screen name="MyRTR" component={MyRTRScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+};
+
+export default App;
